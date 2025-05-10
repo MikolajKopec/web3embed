2f425f59489fa0e63e22e33d1e40b4225f7d49cd
Mikołaj <mikolajkopec772@gmail.com>
2025-05-10 18:50:53 +0700

    add analitics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { inject } from "@vercel/analytics"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  analytics = inject()
 }
